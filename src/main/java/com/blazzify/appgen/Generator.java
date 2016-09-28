4917f739d6cd94219c7dc7b763ef320a3520ccb3
Azzuwan Aziz <azzuwan@gmail.com>
2016-09-29 02:22:38 +0800

    Skeleton Maven project

---
+++ src/main/java/com/blazzify/appgen/Generator.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blazzify.appgen;
+
+/**
+ *
+ * @author azzuwan
+ */
+public class Generator {
+    
+    public static void main(String[] args) {
+        
+    }
+}
