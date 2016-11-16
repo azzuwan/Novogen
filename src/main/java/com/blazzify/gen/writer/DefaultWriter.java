1c53fe2e9ce953db88e76284d92caff845eed65d
Azzuwan Aziz <azzuwan@gmail.com>
2016-11-17 04:49:02 +0800

    Implement test writing block

--- src/main/java/com/blazzify/gen/writer/DefaultWriter.java
+++ src/main/java/com/blazzify/gen/writer/DefaultWriter.java
@@ -23,11 +23,20 @@
  */
 package com.blazzify.gen.writer;
 
-import com.blazzify.gen.model.Project;
-import com.blazzify.gen.model.SparkProject;
+import com.blazzify.gen.project.Project;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.metamodel.schema.Table;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 /**
  *
@@ -44,7 +53,52 @@ public class DefaultWriter<P extends Project> implements Writer{
      
     @Override
     public void write() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String dir = System.getProperty("user.dir");
+        JtwigTemplate template = JtwigTemplate.fileTemplate(dir + "/templates/java/spark/spark.twig");
+        JtwigModel model = JtwigModel.newModel().with("tables", getTables());
+        String generatedPath = project.getPath() + "/projects/" + getProject().getLanguage() + "/" + getProject().getName();
+        String indexFile = generatedPath + "/server.java";
+        System.out.println("Full generation path: " + indexFile);
+        Path path = Paths.get(indexFile);
+        Files.createDirectories(path.getParent());
+        File file = new File(indexFile);
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream(file);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(DefaultWriter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        template.render(model, fos);
     }
+    
+    /**
+     * @return the project
+     */
+    public P getProject() {
+        return project;
+    }
+
+    /**
+     * @param project the project to set
+     */
+    public void setProject(P project) {
+        this.project = project;
+    }
+
+    /**
+     * @return the tables
+     */
+    public List<Table> getTables() {
+        return tables;
+    }
+
+    /**
+     * @param tables the tables to set
+     */
+    public void setTables(List<Table> tables) {
+        this.tables = tables;
+    }
+
     
 }
