7101e29e39aaf5146ff80bd5e89bfceb40697deb
Azzuwan Aziz <azzuwan@gmail.com>
2016-11-18 11:04:32 +0800

    Implement write method for SparkWriter. Change the templates location to resource directory

--- src/main/java/com/blazzify/gen/writer/SparkWriter.java
+++ src/main/java/com/blazzify/gen/writer/SparkWriter.java
@@ -24,23 +24,70 @@
 package com.blazzify.gen.writer;
 
 import com.blazzify.gen.project.Project;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.metamodel.schema.Table;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 /**
  *
  * @author Azzuwan Aziz <azzuwan@gmail.com>
  */
 public class SparkWriter implements Writer{
+    private Project project;   
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public List<Table> getTables() {
+        return tables;
+    }
+
+    public void setTables(List<Table> tables) {
+        this.tables = tables;
+    }
+    private List<Table> tables;
 
     SparkWriter(Project project, List<Table> tables) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.project = project;
+        this.tables = tables;
     }
 
     @Override
     public void write() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String dir = System.getProperty("user.dir");        
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/java/spark/spark.twig");        
+        JtwigModel model = JtwigModel.newModel().with("tables", getTables());
+        String generatedPath = project.getPath() + "/" + getProject().getName();
+        String indexFile = generatedPath + "/server.java";
+        System.out.println("Full generation path: " + indexFile);
+        Path path = Paths.get(indexFile);
+        Files.createDirectories(path.getParent());
+        File file = new File(indexFile);
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream(file);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(DefaultWriter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        template.render(model, fos);
     }
+    
     
 }
