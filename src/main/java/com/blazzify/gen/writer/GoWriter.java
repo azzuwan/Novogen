67e1f2dde92815f9bc8de61a0340055918172a69
Azzuwan Aziz <azzuwan@gmail.com>
2016-11-18 22:13:45 +0800

    Tidying up GoWriter

--- src/main/java/com/blazzify/gen/writer/GoWriter.java
+++ src/main/java/com/blazzify/gen/writer/GoWriter.java
@@ -23,24 +23,83 @@
  */
 package com.blazzify.gen.writer;
 
+import com.blazzify.gen.project.GoProject;
 import com.blazzify.gen.project.Project;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.metamodel.schema.Table;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 /**
  *
  * @author Azzuwan Aziz <azzuwan@gmail.com>
  */
 public class GoWriter implements Writer{
+    private GoProject project;
+    List<Table> tables;
+
+    public GoProject getProject() {
+        return project;
+    }
+
+    public void setProject(GoProject project) {
+        this.project = project;
+    }
+
+    public List<Table> getTables() {
+        return tables;
+    }
+
+    public void setTables(List<Table> tables) {
+        this.tables = tables;
+    }
 
     GoWriter(Project project, List<Table> tables) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.project = (GoProject) project;
+        this.tables = tables;
     }
 
     @Override
     public void write() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String dir = System.getProperty("user.dir");
+        
+        JtwigTemplate template = JtwigTemplate.fileTemplate(dir + "/templates/go/nethttp/server.go");
+        JtwigModel model = JtwigModel.newModel().with("tables", getTables());
+        
+        String generatedPath = project.getPath() + "/" + getProject().getName();
+        
+        String indexFilePathString = generatedPath + "/server.go";
+        String dbFilePathString = generatedPath +"/db.go";
+        System.out.println("Full generation path: " + indexFilePathString);
+        
+        Path indexFilePath = Paths.get(indexFilePathString);
+        Path dbFilePath = Paths.get(dbFilePathString);
+        
+        Files.createDirectories(indexFilePath.getParent());
+        Files.createDirectories(dbFilePath.getParent());
+        
+        File indexFile = new File(indexFilePathString);
+        File dbFile = new File(dbFilePathString);
+        
+        FileOutputStream indexStream = null;
+        FileOutputStream dbStream = null;
+
+        try {
+            indexStream = new FileOutputStream(indexFile);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(DefaultWriter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        template.render(model, indexStream);
+        template.render(model, dbStream);
     }
     
 }
