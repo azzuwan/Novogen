b1d8b9d01fd7be1b97666c3d1bde5bd775b89724
Azzuwan Aziz <azzuwan@gmail.com>
2016-11-21 05:15:32 +0800

    Refactor rename

---
+++ src/main/java/com/blazzify/gen/pebble/filter/CamelCaseFilter.java
@@ -0,0 +1,64 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Azzuwan Aziz <azzuwan@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.blazzify.gen.pebble.filter;
+
+import com.mitchellbosecke.pebble.extension.Filter;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Azzuwan Aziz <azzuwan@gmail.com>
+ */
+public class CamelCaseFilter implements Filter{
+     public static boolean isSnakeCase(String name) {
+        return Pattern.matches("[\\da-z_]+", name);
+    }
+     
+     public static String getCamelCase(String name) {
+        if(name == null) return null;
+
+        Pattern p = Pattern.compile("_(.)");
+        Matcher m = p.matcher(name);
+        StringBuffer sb = new StringBuffer();
+        while (m.find()) {
+            m.appendReplacement(sb, m.group(1).toUpperCase());
+        }
+        m.appendTail(sb);
+        return sb.toString();
+    }
+
+    @Override
+    public Object apply(Object o, Map<String, Object> map) {
+        return getCamelCase(o.toString());
+    }
+
+    @Override
+    public List<String> getArgumentNames() {
+        return null;
+    }
+
+}
